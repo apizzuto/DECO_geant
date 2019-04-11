123d33f9777c2311b6b2c3b4d9e7b484f60153ba
apizzuto <pizzuto@wisc.edu>
2019-04-11 11:51:36 -0500

    added python subprocess interface

--- DECO_allpix/source_shielding/run_subprocess.py
+++ DECO_allpix/source_shielding/run_subprocess.py
@@ -7,7 +7,10 @@ import subprocess
 
 subprocess.call(["ls", "-l"])
 
-particles = ['e+', 'mu+', 'gamma']
+#particles = ['e+', 'mu+', 'gamma']
+particles = ['e-']
+#particles = ['gamma']
+
 
 energies = ['10keV', '31.6keV','100keV', '316keV','1MeV', '3.16MeV','10MeV', '31.6MeV','100MeV', '316MeV',
 		'1GeV', '3.16GeV','10GeV']
@@ -20,7 +23,7 @@ for particle in particles:
         print(my_command)
         my_command += ' DepositionGeant4.particle_type="{}"'.format(particle)
         my_command += ' -o DepositionGeant4.source_energy="{}"'.format(energy)
-        my_command += ' -o ROOTObjectWriter.file_name="{}/{}_theta_90_phi_30.root"'.format(particle, energy)
-        my_command += ' -o TextWriter.file_name="{}/{}_theta_90_phi_30.txt"'.format(particle, energy)
+        #my_command += ' -o ROOTObjectWriter.file_name="{}/{}_theta_120_phi_30.root"'.format(particle, energy)
+        my_command += ' -o TextWriter.file_name="{}/{}_theta_75_phi_30_highstats.txt"'.format(particle, energy)
         #print(my_command)
         subprocess.call(my_command, shell=True)
